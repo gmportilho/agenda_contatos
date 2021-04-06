a0ea699bcf9198238f7f6f7477c1f08dd219be1f
gmportilho <gabrielmastelariportilho@hotmail.com>
2021-04-06 21:19:36 +0000

    Versão da aula do dia 06/04/2021

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require('express');
 const server = express();
 
 server.get('/', function(request, response) {
-    response.send('Hello World!');
+
+    let nome = request.query.nome;
+
+    response.json({nome : nome ?? 'usuário'});
 })
 
 server.listen(process.env.PORT || 3000);
