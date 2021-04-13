eb6cc6f55aaa5ca2996cd29830068c589d945362
gmportilho <gabrielmastelariportilho@hotmail.com>
2021-04-13 21:18:28 +0000

    Ultima mudança da aula

--- server.js
+++ server.js
@@ -2,11 +2,29 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', function(request, response) {
+const database = require('./database');
 
-    let nome = request.query.nome;
+server.use(express.json())
 
-    response.json({nome : nome ?? 'usuário'});
+server.get('/',async function(request, response) {
+    const contatos = await database.read();
+    response.json(contatos);
+})
+
+
+ //localhost:3000/123o9123-123-1-1231233
+ server.get('/:id', function(request, response) {
+     const id = request.params.id;
+     const contato = await database.find(id);
+     response.json(result);
+ })
+
+server.post('/', async function(request, response){
+    const nome = request.body.nome;
+    const telefone = request.body.telefone;
+
+    const result = await database.create(nome, telefone);
+    response.status(201).send();
 })
 
 server.listen(process.env.PORT || 3000);
